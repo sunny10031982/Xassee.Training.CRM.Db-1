e5e31cdce10429437bfbb95799e07bb6fd92127d
sunny10031982 <ravi.march@gmail.com>
2019-07-23 14:45:30 +0100

    Devops

---
+++ Xassee.Training.CRM.Db/Tables/Feedback.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Feedback]
+(
+	[FeedbackId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(255) NULL, 
+    [LastName] VARCHAR(255) NULL, 
+    [Email] VARCHAR(255) NULL, 
+    [Telephone] VARCHAR(80) NULL, 
+    [Title] VARCHAR(255) NULL, 
+    [Description] VARCHAR(MAX) NULL
+)
